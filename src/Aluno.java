5d5af84867c6dc43196bfad98f1a616d743bf611
RuanMiguel <ruan.miguelsc@gmail.com>
2025-10-06 08:33:38 -0300

    Aluno.java

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,4 +1,4 @@
-public class Aluno{
+public class Aluno implements Comparable<Aluno>{
     private String nome;
     private int nota;
     private int matricula;
@@ -23,5 +23,19 @@ public class Aluno{
 
     public String toString() {
         return "Nota: " + nota + "; Nome: " + nome + "; Matrícula: " + matricula;
+    }
+
+    @Override
+    public int compareTo(Aluno outro) {
+        int comparacaoNota = Integer.compare(outro.nota, this.nota);
+        if (comparacaoNota != 0) {
+            return comparacaoNota;
+        }
+
+        int comparacaoNome = this.nome.compareTo(outro.nome);
+        if (comparacaoNome != 0) {
+            return comparacaoNome;
+        }
+        return Integer.compare(this.matricula, outro.matricula);
     }
 }
